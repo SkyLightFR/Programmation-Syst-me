3849cc95cd3096b2fdca8c1fa31aa6e726f0e853
Skylight <chevalier.benjamin.fr@gmail.com>
2015-01-20 16:16:43 +0100

    Working server with replies

--- webserver/main.c
+++ webserver/main.c
@@ -16,7 +16,7 @@ int main(int argc, char **argv) {
 
   int server_socket = creer_serveur(atoi(argv[1]));
   int client_socket;
-  /*char client_message[100];*/
+  char client_message[100];
   while(1) {
 
     client_socket = accept(server_socket, NULL, NULL);
@@ -27,10 +27,10 @@ int main(int argc, char **argv) {
     const char *welcome_message = "Hello ! Welcome on my server! \n Ceci est un nouveau serveur créer par Crosnier Florian et Chevalier Benjamin. \n Il est distribué sous la license GNU GPL V2.0 et a été développé dans le cadre du cours ASR4-Programmation Système.\n\n";
     write(client_socket, welcome_message, strlen(welcome_message));
   
-  /* while(1) {
-    read(client_socket, client_message,100);
-    write(client_socket, client_message,100);
-    }*/
+    while(1) {
+    int lg = read(client_socket, client_message,100);
+    write(client_socket, client_message,lg);
+    }
   }
   return 0;
 }
