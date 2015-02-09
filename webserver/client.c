b07de40b4c76d02e91f4992bbd352dc04b4d1a12
Florian Crosnier <florian.crosnier@etudiant.univ-lille1.fr>
2015-02-09 16:05:34 +0100

    Send welcome message or 404 error

--- webserver/client.c
+++ webserver/client.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
@@ -10,25 +13,54 @@
 int client_treatment(int client_socket) {
     FILE *client = fdopen(client_socket, "w+");
     char client_message[MAX_MSG_LENGTH];
+    int parser;
     int recv_line = 0;
-    const char *error_400 = "HTTP/1.1 400 Bad Request\r\nConnection: close\r\nContent-length: 17\r\n\r\n400 Bad requet\r\n";
-    const char *success_200 = "HTTP/1.1 200 OK\r\nConnection: close\r\nContent-length: 20\r\n\r\n200 Request received\r\n";
 
-    /* Echo everything the client sends */
+    int welcome_fd;
+    struct stat welcome_stat;
+    char welcome_msg[2000];
+
+    const char *response_bad_req = "HTTP/1.1 400 Bad Request\r\nConnection: close\r\nContent-length: 17\r\n\r\n400 Bad request\r\n";
+    const char *response_rsc_not_found = "HTTP/1.1 404 Not Found\r\nConnection: close\r\nContent-length: 15\r\n\r\n404 Not found\r\n";
+    const char *response_ok = "HTTP/1.1 200 OK\r\nConnection: close\r\nContent-length: %d\r\n\r\n%s\r\n";
+
+    /* Fetch everything the client sends until \r\n or \n */
     while (fgets(client_message, MAX_MSG_LENGTH, client)) {
         if (recv_line == 0) {
+            parser = method_parser(client_message);
             ++recv_line;
-            if (method_parser(client_message) < 0) {
-                fprintf(client, error_400);
-                break;
-            }
         }
         if (!strcmp(client_message, "\r\n") || !strcmp(client_message, "\n")) break;
     }
 
-    fprintf(client, success_200);
-    fclose(client);
+    /* Send response according to parser return value */
+    if (parser < 0)
+        fprintf(client, response_bad_req);
+    else if (parser == RSC_NOT_FOUND)
+        fprintf(client, response_rsc_not_found);
+    else {
+        /* Put content of resource/welcome_message in welcome_msg */
+        welcome_fd = open("resource/welcome_message", O_RDONLY);
+        if (welcome_fd == -1) {
+            perror("read resource/welcome_message");
+            strcpy(welcome_msg, "Could not read welcome file\n");
+        } else {
+            if (fstat(welcome_fd, &welcome_stat) == -1) {
+                perror("fstat");
+                strcpy(welcome_msg, "Could not read welcome file\n");
+            } else if (read(welcome_fd, (void *)welcome_msg, welcome_stat.st_size) == -1) {
+                perror("read welcome_message");
+                strcpy(welcome_msg, "Could not read welcome file\n");
+            } else {
+                strcat(welcome_msg, "\0");
+            }
+            close(welcome_fd);
+        }
 
+        fprintf(client, response_ok, strlen(welcome_msg), welcome_msg);
+    }
+
+    fclose(client);
     exit(0);
 }
 
