8c295c8ca328ff19269c62aee82a487bb26af769
Florian Crosnier <florian.crosnier@etudiant.univ-lille1.fr>
2015-02-09 15:38:42 +0100

    Parser validates only GET requests on resource /

--- webserver/http.c
+++ webserver/http.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <regex.h>
+#include "http.h"
 
 regex_t method_regex;
 
@@ -14,32 +15,11 @@ int compile_regex(void) {
 }
 
 int method_parser(const char *request) {
-    /*if (strstr(request, "GET ") != request) return -1;
-
-    regex_t http_version;
-    unsigned int i = 3;
-    int words = 1;
-
-    while (i < strlen(request)) {
-        if (request[i] == ' ') {
-            ++words;
-        }
-        ++i;
-    }
-
-    if (words == 3) {
-        if (regcomp(&http_version, "HTTP/1\\.[01]", 0)) {
-            return -1;
-        } else if (regexec(&http_version, strstr(request, "HTTP/1"), 0, NULL, 0)) {
-            perror("regexec");
-            return -1;
-        }
-    } else {
-        return -1;
-    }*/
     if (regexec(&method_regex, request, 0, NULL, 0) == REG_NOMATCH) {
         return -1;
     }
+
+    if (strncmp(request, "GET / ", 6) != 0) return RSC_NOT_FOUND;
 
     return 0;
 }
--- webserver/http.h
+++ webserver/http.h
@@ -1,2 +1,4 @@
+#define RSC_NOT_FOUND 404
+
 int method_parser(const char *request);
 int compile_regex(void);
