d340a916d9598d84693486534489445b169c6104
Florian Crosnier <florian.crosnier@etudiant.univ-lille1.fr>
2015-02-15 20:40:21 +0100

    Rewrite URL to remove query part

--- webserver/http.c
+++ webserver/http.c
@@ -3,7 +3,9 @@
 #include "acquire.h"
 #include "http.h"
 
-/* Fill *request according to *request_line */
+char *rewrite_url(char *url);
+
+/* Fills *request according to *request_line */
 int parse_http_request(const char *request_line, http_request *request) {
     char rq_method[7];
     char rq_url[2000];
@@ -13,7 +15,7 @@ int parse_http_request(const char *request_line, http_request *request) {
     /* request_line must contain three non-empty strings */
     if (sscanf(request_line, "%s %s %s %c", rq_method, rq_url, rq_version, &devnull) != 3) return -1;
 
-    request->url = rq_url;
+    request->url = rewrite_url(rq_url);
 
     if (!strcmp(rq_method, "GET")) request->method = HTTP_GET;
     else request->method = HTTP_UNSUPPORTED;
@@ -34,13 +36,30 @@ int parse_http_request(const char *request_line, http_request *request) {
     return 0;
 }
 
+/* Ignores the headers in the client request */
 void skip_headers(FILE *client) {
     char client_line[MAX_MSG_LENGTH];
 
-    /* Ignore the headers in the client request */
     while (1) {
         fgets_or_exit(client_line, MAX_MSG_LENGTH, client);
         /* Wait for the end... just wait */
         if (!strcmp(client_line, "\r\n") || !strcmp(client_line, "\n")) break;
     }
+}
+
+/* Removes the query part of the URL */
+char *rewrite_url(char *url) {
+    char formatted_url[2000];
+    unsigned int i;
+
+    /* Find the first position of the '?' symbol in the URL */
+    for (i = 0 ; i < strlen(url) && url[i] != '?' ; ++i);
+
+    /* Re-create the URL if '?' was found */
+    if (i != strlen(url)) {
+        strncpy(formatted_url, url, i);
+        strcpy(url, formatted_url);
+    }
+
+    return url;
 }
